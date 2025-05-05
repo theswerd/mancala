0192380e48ee3ba6ac88f58dd20dbf280f448243
SpeckyYT <specky.fans@gmail.com>
2025-05-05 22:45:47 +0200

    improved C&ET performance and readability
    Co-Authored-By: kr8gz <47776344+kr8gz@users.noreply.github.com>

--- bank_bird/src/algorithm/mix.rs
+++ bank_bird/src/algorithm/mix.rs
@@ -1,4 +1,5 @@
 use super::Algorithm;
+use arrayvec::ArrayVec;
 use mancala_board::{MancalaBoard, Side, MoveResult};
 
 #[derive(Debug, Clone, Copy)]
@@ -7,18 +8,15 @@ impl<const S: usize> Algorithm<S> for CaptureAndExtraTurn {
     fn min_games(&self) -> usize { 1 }
     fn name(&self) -> String { "Capture & Extra Turn".to_string() }
     fn play_move(&mut self, board: &MancalaBoard<S>, side: Side) -> usize {
-        let mut captures = vec![];
-        let mut extra_turns = vec![];
+        let mut captures: ArrayVec<(usize, u32), S> = ArrayVec::new();
+        let mut extra_turns: ArrayVec<usize, S> = ArrayVec::new();
 
         for i in 0..S {
             let mut board = *board;
             let result = board.move_piece_kalah(side, i);
             match result {
                 MoveResult::Capture(_, _) => {
-                    let mut profit = *board.side_bank(side);
-                    board.capture_kalah(side, i);
-                    profit = *board.side_bank(side) - profit;
-
+                    let profit = board.side_to_dishes(side)[i] + board.side_to_dishes(!side)[board.opposite_dish_index(i)];
                     captures.push((i, profit))
                 }
                 MoveResult::ExtraTurn => extra_turns.push(i),
@@ -27,21 +25,13 @@ impl<const S: usize> Algorithm<S> for CaptureAndExtraTurn {
         }
 
         // capture the biggest if possible
-        if !captures.is_empty() {
-            let a = captures.iter().reduce(|a, b| {
-                if b.1 > a.1 {
-                    b
-                } else {
-                    a
-                }
-            }).unwrap();
-
-            return a.0
+        if let Some((i, _)) = captures.into_iter().max_by_key(|x| x.1) {
+            return i;
         }
 
         // get an extra turn if possible
-        if !extra_turns.is_empty() {
-            return *extra_turns.last().unwrap()
+        if let Some(&i) = extra_turns.last() {
+            return i;
         }
 
         // pick the last move
